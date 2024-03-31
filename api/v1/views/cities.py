014fe2d58ebac70cec1fad755a9753c85f4cdfa1
DevBasem <engr.basemahmed@gmail.com>
2024-03-31 16:25:08 -0700

    task 7 fx

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -32,7 +32,7 @@ def delete_city(city_id):
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    storage.delete(city)
+    city.delete()
     storage.save()
     return jsonify({})
 
